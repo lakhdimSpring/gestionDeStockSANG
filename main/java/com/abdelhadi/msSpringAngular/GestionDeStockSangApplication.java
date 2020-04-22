606109c50f7b51cb214eb3c4835b69445689e373
unknown <abdelhadilakhdim@gmail.com>
2020-04-22 22:31:19 +0000

    commit action

---
+++ main/java/com/abdelhadi/msSpringAngular/GestionDeStockSangApplication.java
@@ -0,0 +1,13 @@
+package com.abdelhadi.msSpringAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStockSangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeStockSangApplication.class, args);
+	}
+
+}
---
+++ test/java/com/abdelhadi/msSpringAngular/GestionDeStockSangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelhadi.msSpringAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockSangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
